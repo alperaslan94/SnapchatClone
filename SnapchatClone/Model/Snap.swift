4e5846263d9ad90eaa2e886c9f4ddd3c11a5c777
alperaslan94 <125819817+alperaslan94@users.noreply.github.com>
2023-10-01 15:47:12 -0700

    SnapchatClone

---
+++ SnapchatClone/Model/Snap.swift
@@ -0,0 +1,16 @@
+//
+//  Snap.swift
+//  SnapchatClone
+//
+//  Created by Alperaslan on 30.09.2023.
+//
+
+import Foundation
+
+
+struct Snap {
+    var username : String
+    var imageUrlArray : [String]
+    var date : Date
+    var timeDifference : Int
+}
---
+++ SnapchatClone/Model/UserSingleton.swift
@@ -0,0 +1,28 @@
+//
+//  UserSingleton.swift
+//  SnapchatClone
+//
+//  Created by Alperaslan on 30.09.2023.
+//
+
+import Foundation
+
+
+class UserSingleton {
+    
+    static let sharedUserInfo = UserSingleton()
+    
+    var email = ""
+    var username = ""
+    
+    
+    
+    
+    
+    
+    
+    private init() {
+        
+    }
+    
+}
